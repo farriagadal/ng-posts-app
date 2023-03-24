719806a365978fac174a3798fb9f8b2556111feb
Fernando <farriagadal94@gmail.com>
2023-03-24 12:25:35 -0300

    add background style

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -9,17 +9,17 @@ export class PostService {
     {
       id: 1,
       name: 'Post 1',
-      description: 'Este es el primer post de ejemplo.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
     },
     {
       id: 2,
       name: 'Post 2',
-      description: 'Este es el segundo post de ejemplo.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
     },
     {
       id: 3,
       name: 'Post 3',
-      description: 'Este es el tercer post de ejemplo.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
     },
   ];
 
